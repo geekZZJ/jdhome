647e7ba4505fd3fb31fd36e06dda1ee9c528f14a
jayzjzhang <jayzjzhang@tencent.com>
2021-11-23 17:45:36 +0800

    路由传参获取及渲染

--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -7,14 +7,14 @@
         <input class="search__content__input" placeholder="请输入商品名称" />
       </div>
     </div>
-    <ShopInfo :nearbyList="data.item" :hideBorder="true"></ShopInfo>
+    <ShopInfo :nearbyList="item" :hideBorder="true"></ShopInfo>
   </div>
 </template>
 
 <script>
 import ShopInfo from '@/components/ShopInfo'
-import { useRouter } from 'vue-router'
-import { reactive } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { reactive, toRefs } from 'vue'
 
 const handleBackEffect = () => {
   const router = useRouter()
@@ -23,60 +23,70 @@ const handleBackEffect = () => {
   }
   return { handleBackClick }
 }
+
+// 获取当前商铺信息
+const useShopInfoEffect = () => {
+  const data = reactive({ item: [] })
+  const route = useRoute()
+  // 获取到路由中的id
+  // console.log('route', route.params.id)
+  data.item = [
+    {
+      id: route.params.id,
+      title: `沃尔玛${route.params.id}`,
+      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
+      tags: ['月售1万+', '起送¥0', '基础运费¥5'],
+      desc: 'VIP尊享满89元减4元运费券（每月3张）'
+    }
+  ]
+  const { item } = toRefs(data)
+  return { item }
+}
 export default {
   name: 'Shop',
   components: { ShopInfo },
   setup () {
     const { handleBackClick } = handleBackEffect()
-    const data = reactive({ item: [] })
-    data.item = [
-      {
-        id: 1,
-        title: '沃尔玛',
-        imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
-        tags: ['月售1万+', '起送¥0', '基础运费¥5'],
-        desc: 'VIP尊享满89元减4元运费券（每月3张）'
-      }
-    ]
+    const { item } = useShopInfoEffect()
 
-    return { data, handleBackClick }
+    return { item, handleBackClick }
   }
 }
 </script>
 
 <style scoped lang="scss">
 @import "../../style/variable";
-.wrapper{
-  padding: 0 .18rem;
-  .search{
-    margin: .2rem 0 .04rem 0;
+.wrapper {
+  padding: 0 0.18rem;
+  .search {
+    margin: 0.2rem 0 0.04rem 0;
     display: flex;
-    &__back{
-      width: .3rem;
-      line-height: .32rem;
-      font-size: .24rem;
+    &__back {
+      width: 0.3rem;
+      line-height: 0.32rem;
+      font-size: 0.24rem;
       color: #b6b6b6;
     }
-    &__content{
+    &__content {
       flex: 1;
       background-color: $search-bgColor;
-      border-radius: .16rem;
+      border-radius: 0.16rem;
       display: flex;
-      &__icon{
-        width: .44rem;
-        line-height: .32rem;
+      &__icon {
+        width: 0.44rem;
+        line-height: 0.32rem;
         text-align: center;
         color: $search-fontColor;
       }
-      &__input{
+      &__input {
         border: none;
         outline: none;
         background-color: transparent;
         width: 100%;
-        padding-right: .2rem;
+        padding-right: 0.2rem;
         color: $content-fontcolor;
-        font-size: .14rem;
-        &::placeholder{
+        font-size: 0.14rem;
+        &::placeholder {
           color: $content-fontcolor;
         }
       }
