f3842ed99452571e4e44f1d0bcde85075b3da1e0
zzj <2461927976@qq.com>
2021-11-28 14:55:11 +0800

    购物车样式

--- src/views/shop/Cart.vue
+++ src/views/shop/Cart.vue
@@ -1,8 +1,9 @@
+<link rel="stylesheet" href="../../style/variable.scss">
 <template>
   <div class="cart">
     <div class="check">
       <div class="check__icon">
-        <img src="http://www.dell-lee.com/imgs/vue3/cart.png" alt="" class="check__icon__img">
+        <img src="http://www.dell-lee.com/imgs/vue3/basket.png" alt="" class="check__icon__img">
         <div class="check__icon__tag">1</div>
       </div>
       <div class="check__info">
@@ -20,12 +21,61 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "../../style/variable";
 .cart{
   position: absolute;
   left: 0;
   right: 0;
   bottom: 0;
-  height: .5rem;
-  background-color: green;
+  .check{
+    display: flex;
+    height: .49rem;
+    border-top: .01rem solid $content-bgColor;
+    &__icon{
+      width: .84rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      &__img{
+        width: .28rem;
+        height: .26rem;
+      }
+      &__tag{
+        position: absolute;
+        width: .2rem;
+        height: .2rem;
+        line-height: .2rem;
+        background-color: $highlight-fontColor;
+        border-radius: 50%;
+        font-size: .12rem;
+        text-align: center;
+        right: .2rem;
+        top: .04rem;
+        color: $bg-color;
+        transform: scale(.5);
+      }
+    }
+    &__info{
+      flex: 1;
+      color: $content-fontcolor;
+      font-size: .12rem;
+      display: flex;
+      align-items: center;
+      &__price{
+        color: $highlight-fontColor;
+        font-size: .18rem;
+      }
+    }
+    &__btn{
+      width: .98rem;
+      background-color: #4fb0f9;
+      color: $bg-color;
+      font-size: .14rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
 }
 </style>
